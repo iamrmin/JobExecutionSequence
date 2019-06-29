6f9771a4b46350d1123cb88d0882cfc8a6e0131f
Rashmin Javiya <rashmin.javiya@siemens.com>
2019-06-29 16:10:11 +0530

    feat(job): Accept file name/path as an cmd arg and added JobDependencyService to parse the raw data

---
+++ src/JobExecutionSequence/JobDependencyService.cs
@@ -0,0 +1,38 @@
+﻿namespace JobExecutionSequence
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Manage job dependency
+    /// </summary>
+    public class JobDependencyService
+    {
+        /// <summary>
+        /// Hold data structure of job and it dependencies
+        /// </summary>
+        private Dictionary<string, string> jobs;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="rawString">The raw input</param>
+        public JobDependencyService(string rawString)
+        {
+            this.RawDataParser(rawString);
+        }
+
+        private void RawDataParser(string rawString)
+        {
+            var rawJobs = rawString.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            this.jobs = new Dictionary<string, string>();
+
+            foreach (var job in rawJobs)
+            {
+                var splitData = job.Split("=>", StringSplitOptions.RemoveEmptyEntries);
+                this.jobs.Add(splitData[0], splitData.Length > 1 ? splitData[1] : null);
+            }
+        }
+    }
+}
---
+++ src/JobExecutionSequence/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace JobExecutionSequence
+{
+    using System;
+    using System.IO;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                throw new ArgumentNullException($"No file path given");
+
+            var rawFileText = File.ReadAllText(args[0]);
+            var jobDependencyService = new JobDependencyService(rawFileText);
+        }
+    }
+}
